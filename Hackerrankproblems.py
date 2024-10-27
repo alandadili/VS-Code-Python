7b45bb04034e34d81cd4ff5a296c290352da910a
alandadili <alandadili123@gmail.com>
2024-10-27 14:20:33 -0400

    problem 2

--- Hackerrankproblems.py
+++ Hackerrankproblems.py
@@ -1,11 +1,26 @@
-if __name__ == '__main__':
-    n = int(input().strip())
-    if n%2!=0:
-        print ("Weird")
-    if n % 2 == 0 and n in range(2, 6):
-         print ("Not Weird")
-    if n%2==0 and n in range(6,21):
-         print ("Weird")
-    if n%2==0 and n>20:
-         print ("Not Weird")
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#     if n%2!=0:
+#         print ("Weird")
+#     if n % 2 == 0 and n in range(2, 6):
+#          print ("Not Weird")
+#     if n%2==0 and n in range(6,21):
+#          print ("Weird")
+#     if n%2==0 and n>20:
+#          print ("Not Weird")
+
+# def is_leap(year):
+#     leap = False
+    
+#     # Write your logic here
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 leap = True
+#         else:
+#             leap = True
+#     return leap
+
+# year = int(input("Enter a year: "))
+# print(is_leap(year))
         
